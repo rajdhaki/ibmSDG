fb7c7d502bf58de6d1e55ed81b30d3a157772e3b
rajdhaki <rajdhakibdr@gmail.com>
2024-07-08 00:51:51 +0530

    commit

--- src/elements/Cursor.jsx
+++ src/elements/Cursor.jsx
@@ -4,9 +4,6 @@ import { useGSAP } from '@gsap/react'
 import { useState } from 'react'
 
 const Cursor = () => {
-  
-
-
     return (
         <div  id="main" style={{"width":"100vw", "height":"100vh"}}>
         <div id='cursor' className='w-2.5 h-2.5 bg-blue-950 rota rounded-full fixed'></div>
