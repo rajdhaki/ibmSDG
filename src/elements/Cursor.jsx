4cf7a9219ec2b154a666644b879860cb02f0d0ef
rajdhaki <rajdhakibdr@gmail.com>
2024-07-08 11:53:04 +0530

    commit

--- src/elements/Cursor.jsx
+++ src/elements/Cursor.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
-import { useState } from 'react'
+import { useRef } from 'react'
 
 const Cursor = () => {
+    var main = useRef(null)
+    const cursor = useRef(null)
+
+    useGSAP(() => {
+        main.current.addEventListener('mousemove', e => {
+            gsap.to(cursor.current, {
+                x: e.x,
+                y: e.y,
+                ease: "back.out(1.7)",
+                duration: 1
+            })
+        })
+    })
+
     return (
-        <div  id="main" style={{"width":"100vw", "height":"100vh"}}>
-        <div id='cursor' className='w-2.5 h-2.5 bg-blue-950 rota rounded-full fixed'></div>
+        <div ref={main} id="main" style={{ "width": "100vw", "height": "100vh" }}>
+            <div ref={cursor} id='cursor' className='w-2.5 h-2.5 bg-blue-950 rota rounded-full fixed'></div>
         </div>
     )
 }
