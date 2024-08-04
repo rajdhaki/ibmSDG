c7c32b3f6434e708b12345fa7cc740f2f2aa84aa
Divyang <divyang0024@gmail.com>
2024-08-04 18:37:47 +0530

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,69 +1,99 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { FaBars } from 'react-icons/fa';
-import Logo from '../Images/logo.webp'
-import { TweenMax, Power3 } from 'gsap';
-import gsap from 'gsap';
-import { NavLink } from 'react-router-dom';
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const Navbar = (() => {
-    // let textItem = useRef(null);
-    // let navItem = useRef(null);
-    const [open, setOpen] = useState(false)
-    const Menus = ["Home", "News", "Education", "About us"]
+const navigation = [
+  { name: "home", href: "/", current: false },
+  { name: "Ranking", href: "/rank", current: false },
+  { name: "Educational", href: "/reports", current: false },
+  { name: "News", href: "/news", current: false },
+  { name: "About", href: "/aboutus", current: false },
+];
 
-    // gsap.registerPlugin(TweenMax)
-    //     useEffect(()=>{
-    //         TweenMax.from(textItem,.4,{ocapcity:0,x:40,ease:Power3.easeOut,delay:.4})
-    //         TweenMax.from(navItem,.4,{ocapcity:0,x:40,ease:Power3.easeOut,delay:.6})
-    //     },[])
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
-    return (
-        <div className='flex justify-between items-center px-16 backdrop-blur-md sticky top-0 left-0 z-30' id='parent'>
-            <div className='flex items-center gap-3 backdrop-blur-lg '>
-                <div className='backdrop-blur-lg' >
-                    <img src={Logo} alt='logo' className='h-16 cursor-pointer transform hover:scale-125 transition-all duration-150 ease-in-out' />
-                </div>
-                <div className='backdrop-blur-lg' >
-                    <p className='text-3xl font-extrabold px-3 tracking-tight text-center cursor-pointer hover:underline hover:text-blue-700
-                transform hover:scale-125 transition-all duration-150 ease-in-out ' >SDG</p>
-                </div>
+export default function Navbar() {
+  return (
+    <Disclosure
+      as="nav"
+      className="bg-transparent  sticky top-0 w-screen z-50 p-2 backdrop-blur-lg"
+    >
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-[#101F77]  hover:text-[#101F77] outline-none ring-[#101F77]">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon
+                aria-hidden="true"
+                className="block h-6 w-6 group-data-[open]:hidden"
+              />
+              <XMarkIcon
+                aria-hidden="true"
+                className="hidden h-6 w-6 group-data-[open]:block"
+              />
+            </DisclosureButton>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-center sm:justify-between">
+            <div className="flex flex-shrink-0 items-center">
+              <img
+                src="https://freepngtransparent.com/wp-content/uploads/2023/03/earth-png-120.png"
+                alt=""
+                className="w-16 h-16"
+              />
             </div>
-            <div >
-                <ul className='hidden md:flex md:items-center md:gap-10
-                text-xl font-extrabold cursor-pointer'>
-                    <li className=' py-1 px-2
-                     hover:bg-slate-400  hover:rounded-full '><NavLink to="/">Home</NavLink></li>
-                    <li className=' py-1 px-2
-                     hover:bg-slate-400 hover:rounded-full '><NavLink to="/rank">Ranking</NavLink></li>
-                    <li className=' py-1 px-2
-                     hover:bg-slate-400 hover:rounded-full '><NavLink to="/reports">Reports</NavLink></li>
-                    <li className=' py-1 px-2
-                     hover:bg-slate-400  hover:rounded-full '><NavLink to="/news">News</NavLink></li>
-                    <li className=' py-1 px-2
-                     hover:bg-slate-400 hover:rounded-full'><NavLink to="/aboutus">Aboutus </NavLink></li>
-                    <li className=' py-1 px-2
-                     hover:bg-slate-400 hover:rounded-full'><NavLink to="/contact">Contactus </NavLink></li>
-                </ul>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? "page" : undefined}
+                    className={classNames(
+                      item.current
+                        ? "bg-transparent text-[#101F77] font-bold text-2xl"
+                        : "text-[#101F77]",
+                      "rounded-md px-3 py-2 font-bold text-xl"
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
             </div>
-            {
-                open && <div className="bg-orange p-4 h-screen w-64 shadow-lg absolute top-20 -right-1 font-sans font-medium tracking-wider">
-                    <ul>
-                        {
-                            Menus.map((menu) => (
-                                <li onClick={() => setOpen(false)}
-                                    className=' text-lg cursor-pointer rounded  ' key={menu}>{menu}</li>
-                            ))
-                        }
-                    </ul>
-                </div>
-            }
-            <div className='lg:hidden'>
-                <FaBars className='hover:border-double border-2 border-black ring-offset-8 h-9 w-10 mr-6 rounded' id='drop-down'
-                    onClick={() => setOpen(!open)} />
-
-            </div>
+          </div>
         </div>
-    )
-})
-export default Navbar;
+      </div>
 
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? "page" : undefined}
+              className={classNames(
+                item.current
+                  ? "bg-transparent text-[#101F77]"
+                  : "text-[#101F77] hover:text-[#101F77]",
+                "block rounded-md px-3 py-2 text-base font-medium"
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  );
+}
